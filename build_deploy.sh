da642d5bcfa140568fc4b7e1dbe3089099031c98
Gavin Romig-Koch <gavin@redhat.com>
2019-08-28 11:38:47 -0400

    build_deploy.sh copied largely from https://github.com/fabric8-launcher/launcher-quarkus.git

--- build_deploy.sh
+++ build_deploy.sh
@@ -1,2 +1,17 @@
 #!/bin/bash
-echo "I also do nothing right now."
+
+set -exv
+
+IMAGE="quay.io/cloudservices/approval-api"
+IMAGE_TAG=$(git rev-parse --short=7 HEAD)
+
+docker build --no-cache -t "${IMAGE}:${IMAGE_TAG}" .
+
+if [[ -n "$QUAY_USER" && -n "$QUAY_TOKEN" ]]; then
+    DOCKER_CONF="$PWD/.docker"
+    mkdir -p "$DOCKER_CONF"
+    docker tag "${IMAGE}:${IMAGE_TAG}" "${IMAGE}:latest"
+    docker --config="$DOCKER_CONF" login -u="$QUAY_USER" -p="$QUAY_TOKEN" quay.io
+    docker --config="$DOCKER_CONF" push "${IMAGE}:${IMAGE_TAG}"
+    docker --config="$DOCKER_CONF" push "${IMAGE}:latest"
+fi
