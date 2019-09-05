a523607ef5a459a2c3664fcf37fbea62eb14bae7
Brandon Squizzato <bsquizza@redhat.com>
2019-09-05 10:41:42 -0400

    Fix quay docker config json

--- build_deploy.sh
+++ build_deploy.sh
@@ -9,10 +9,8 @@ docker build --no-cache -t "${IMAGE}:${IMAGE_TAG}" .
 
 
 if [[ -n "$QUAY_USER" && -n "$QUAY_TOKEN" ]]; then
-    DOCKER_CONF="$PWD/.docker"
-    mkdir -p "$DOCKER_CONF"
     docker tag "${IMAGE}:${IMAGE_TAG}" "${IMAGE}:latest"
-    docker --config="$DOCKER_CONF" login -u="$QUAY_USER" -p="$QUAY_TOKEN" quay.io
-    docker --config="$DOCKER_CONF" push "${IMAGE}:${IMAGE_TAG}"
-    docker --config="$DOCKER_CONF" push "${IMAGE}:latest"
+    docker --config="$QUAY_DOCKER_CONFIG_JSON" login -u="$QUAY_USER" -p="$QUAY_TOKEN" quay.io
+    docker --config="$QUAY_DOCKER_CONFIG_JSON" push "${IMAGE}:${IMAGE_TAG}"
+    docker --config="$QUAY_DOCKER_CONFIG_JSON" push "${IMAGE}:latest"
 fi
