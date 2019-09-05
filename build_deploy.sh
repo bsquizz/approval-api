fb65309fed23b151746ed436dd03789230f32e60
Brandon Squizzato <bsquizza@redhat.com>
2019-09-05 10:55:40 -0400

    Store docker config json in a file

--- build_deploy.sh
+++ build_deploy.sh
@@ -10,7 +10,13 @@ docker build --no-cache -t "${IMAGE}:${IMAGE_TAG}" .
 
 if [[ -n "$QUAY_USER" && -n "$QUAY_TOKEN" ]]; then
     docker tag "${IMAGE}:${IMAGE_TAG}" "${IMAGE}:latest"
-    docker --config="$QUAY_DOCKER_CONFIG_JSON" login -u="$QUAY_USER" -p="$QUAY_TOKEN" quay.io
-    docker --config="$QUAY_DOCKER_CONFIG_JSON" push "${IMAGE}:${IMAGE_TAG}"
-    docker --config="$QUAY_DOCKER_CONFIG_JSON" push "${IMAGE}:latest"
+
+    # set +x so we don't echo the json to stdout
+    set +x
+    echo "$QUAY_DOCKER_CONFIG_JSON" > .docker_config.json
+    set -x
+
+    docker --config=".docker_config.json" login -u="$QUAY_USER" -p="$QUAY_TOKEN" quay.io
+    docker --config=".docker_config.json" push "${IMAGE}:${IMAGE_TAG}"
+    docker --config=".docker_config.json" push "${IMAGE}:latest"
 fi
